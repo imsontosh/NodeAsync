7c1ca896aa27d58017b42e41e41e95954eed9918
Santosh Kumar Nayak <imsontosh@gmail.com>
2017-05-22 15:28:46 +0530

    Update filewrite.js
    updated with indentation

--- filewrite.js
+++ filewrite.js
@@ -1,17 +1,17 @@
-function doSomethingOnceAllAreDone(){
-    console.log("Everything is done.");
+function doSomethingOnceAllAreDone() {
+    console.log('Everything is done.');
 }
 
-function Item(delay){
+function Item(delay) {
     this.delay = delay;
 }
 
-Item.prototype.someAsyncCall = function(callback){
+Item.prototype.someAsyncCall = function(callback) {
     var temp = this;
     console.log('Item Started', temp.delay);
-    setTimeout(function(){
-        console.log("Item is done. -------",temp.delay);
-        if(typeof callback === "function") callback();
+    setTimeout(function() {
+        console.log('Item is done. -------', temp.delay);
+        if (typeof callback === 'function') callback();
     }, this.delay);
 };
 
@@ -20,20 +20,26 @@ items.push(new Item(1000));
 items.push(new Item(200));
 items.push(new Item(500));
 
-
-var async = require("async");
- 
+var async = require('async');
+const fse = require('fs-extra');
 var asyncTasks = [];
- 
-items.forEach(function(item){
-  asyncTasks.push(function(callback){
-    item.someAsyncCall(function(){
-        console.log("Item is done. -------",item.delay);
-        callback();
+var outPath = './build/';
+var name = 'file';
+var ext = 'txt';
+
+items.forEach(function(item) {
+    asyncTasks.push(function(callback) {
+        item.someAsyncCall(function() {
+            try {
+                fse.outputFileSync(outPath + name + item.delay + '.' + ext, item.delay);
+            } catch (error) {
+                console.log('Error writing Component file ' + name + ': ' + error);
+            }
+            callback();
+        });
     });
-  });
 });
- 
-async.parallel(asyncTasks, function(){
-  doSomethingOnceAllAreDone();
+
+async.parallel(asyncTasks, function() {
+    doSomethingOnceAllAreDone();
 });
